138a5e9c799144eeb6f86055713e08826cd3c60f
HERIKA SOUSA FELIPE DA SILVA <HERIKA0565@DIGITALCOLLEGE.LOCAL>
2024-03-16 11:53:28 -0300

    aula 16-03

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 import express, { request, response } from "express"
 import sequelize from "./src/database/sequelize.js"
 import User from "./src/models/user.js"
+import { connection } from "./src/database/mysql-connect.js"
+
 
 const app = express()
 const router = express.Router()
+
+app.get('programming-languages', async (request, response) => {
+    const data = [
+        {id: 1, language: 'Linguagem 1', percentage: 30}
+    ]
+
+    const data = await connection.execute('select * from programming_laguages')
+
+    return response.json(data)
+})
+
+app.post()
 
 router.get("/", async (request, response) => {
 
